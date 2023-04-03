7c2648cceb6403830f71b622d7cee9bd4c4ad86c
Kg-Deva <kangdeva2002@gmail.com>
2023-04-03 08:59:05 +0700

    dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -46,7 +46,7 @@
                 class="sidebar-item active ">
                 <a href="index.html" class='sidebar-link'>
                     <i class="bi bi-grid-fill"></i>
-                    <span>Dashboard</span>
+                    <span>Dashboardd</span>
                 </a>
             </li>
             
