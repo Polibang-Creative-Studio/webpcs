27d29d9014e743f1dec72da7decc769c28b446fd
Kg-Deva <kangdeva2002@gmail.com>
2023-04-08 15:05:36 +0700

    perbaiki typo, register

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -19,7 +19,7 @@
     <div class="col-lg-5 col-12">
         <div id="auth-left">
             <div class="auth-logo">
-                <a href="index"><img src="assets/admin/images/logo/logo.svg" alt="Logo"></a>
+                <a href="index"><img src="assets/img/logopcs.png" alt="Logo"></a>
             </div>
             <h1 class="auth-title">Log in.</h1>
             <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p>
@@ -45,11 +45,11 @@
                 </div>
                 <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
             </form>
-            <div class="text-center mt-5 text-lg fs-4">
+            <!-- <div class="text-center mt-5 text-lg fs-4">
                 <p class="text-gray-600">Don't have an account? <a href="register" class="font-bold">Sign
                         up</a>.</p>
                 <p><a class="font-bold" href="auth-forgot-password.html">Forgot password?</a>.</p>
-            </div>
+            </div> -->
         </div>
     </div>
     <div class="col-lg-7 d-none d-lg-block">
